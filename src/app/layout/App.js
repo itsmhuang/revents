457b010a454dd9342447f8e1028af276bd83b4b2
Michelle Huang <huangmhuang@gmail.com>
2019-05-21 18:54:46 -0700

    add jsconfig; other components

--- src/app/layout/App.js
+++ src/app/layout/App.js
@@ -1,9 +1,13 @@
 import React from "react";
+import EventDashboard from "src/features/event/EventDashboard";
+import NavBar from "src/features/nav/NavBar";
 
 function App() {
   return (
     <div>
       <h1>Re-vents</h1>
+      <NavBar/>
+      <EventDashboard/>
     </div>
   );
 }
